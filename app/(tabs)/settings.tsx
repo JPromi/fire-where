c073a3ab0f1f2bfb8f90a669359694f4ea2f80a5
JPromi <jonasmpr05@gmail.com>
2025-06-18 07:05:08 +0200

    add android folder

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,5 +1,10 @@
 
 
 export default function SettingsScreen() {
-  
+  /*
+  ToDo:
+  - Change language
+  - Activate nerd mode (display operation ids, uuids, system informations,...)
+  - Activate jump to district / federal state
+  */
 }
