f1152fccb24373928b359d98c945c90c309c7fc5
JPromi <jonasmpr05@gmail.com>
2025-08-03 11:04:41 +0200

    remove nested routes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -109,7 +109,7 @@ export default function TabLayout() {
           },
         })}
       />
-      <Tabs.Screen
+      {/* <Tabs.Screen
         name="operation/[federalStateId]"
         options={{ href: null }}
       />
@@ -124,7 +124,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="settings/[settingKey]"
         options={{ href: null }}
-      />
+      /> */}
       <Tabs.Screen
         name="index"
         options={{ href: null }}
